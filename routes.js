0a3b23fb9462dda12ca0557389eaab1c78ea0c23
Joao Victor <joao.teixeira@globaldotcom.con.br>
2023-11-21 12:44:06 -0300

    merge

--- routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import CustomerController from './src/app/controller/customerController.js'; 
-const router = Router();
-
-router.get('/customers', CustomerController.index);
-router.get('/customers/:id', CustomerController.show);
-router.post('/customers/', CustomerController.update);
-
-export default router;
